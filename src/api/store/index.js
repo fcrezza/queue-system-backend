8dde737dcbf6786f1db18a253dad873ee0bb93e7
Anang Fachreza <fachrezanang@gmail.com>
2020-07-23 16:18:57 +0700

    fix error

--- src/api/store/index.js
+++ src/api/store/index.js
@@ -1,6 +1,28 @@
 const mysql = require('mysql')
+
 const connectionOption = require('./connectionOption')
 
-const connection = mysql.createConnection(connectionOption)
+let connection
+function connectDB() {
+	connection = mysql.createConnection(connectionOption)
+
+	connection.connect((err) => {
+		if (err) {
+			console.log('error when connecting to db:', err)
+			setTimeout(connectDB, 2000)
+		}
+	})
+
+	connection.on('error', (err) => {
+		console.log('db error', err)
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			connectDB()
+		} else {
+			throw err
+		}
+	})
+}
+
+connectDB()
 
 module.exports = {connection, mysql}
